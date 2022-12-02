f932d7230a0e33a6a02fc9f8f4c0314b26d46dfe
SergiyBrotsky <49614370+BrotskyS@users.noreply.github.com>
2022-12-02 11:44:35 +0200

    update UI in auth screen

---
+++ Where/Helpers/Extensions/String + ext.swift
@@ -0,0 +1,8 @@
+//
+//  String + ext.swift
+//  Where
+//
+//  Created by Sergiy Brotsky on 01.12.2022.
+//
+
+import Foundation
---
+++ Where/Managers/ValidationManager.swift
@@ -0,0 +1,8 @@
+//
+//  ValidationManager.swift
+//  Where
+//
+//  Created by Sergiy Brotsky on 01.12.2022.
+//
+
+import Foundation
